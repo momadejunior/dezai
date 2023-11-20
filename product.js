5051acf38735e665619dab3a6b1076b905bff5e8
Momade Junior <momadejunior584@gmail.com>
2023-11-20 11:32:12 -0800

    Update product.js

--- product.js
+++ product.js
@@ -1,25 +1,25 @@
 let products = [
             {
-                name: "Long Cartigen",
+                name: "Blusa Sádia",
                 badge: true,
                 color: ["abbey", "blue", "Copper", "Old Rose"],
                 size: ["XL", "L", "M", "S"],
                 images: ["img-01.png", "img-02.png", "img-03.png", "img-04.png"],
                 price: 100,
-                description:"longe cartigen for cold day...",
+                description:"Blusa Sádia: Blusa com design de manga cava, decote em V e detalhe de costas cruzadas, confeccionada em mistuta de  linho e algodão.",
                 
             },
             {
-                name: "Short Cartigen",
+                name: "Calça Sádia",
                 badge: false,
                 color: ["red", "green", "black", "white"],
                 size: ["M", "L", "XL"],
                 images: ["img-05.png", "img-06.png", "img-07.png", "img-08.png"],
                 price: 56,
-                 description:"longe cartigen for cold day...",
+                 description:"Calça Sádia: Calças confecionadas em tecido com mistura de linho e algodão.Cintura subida com bolsos  à frente. Perna direita.Fecho de correr e botão forrado à frente.",
             },
             {
-                name: "Sporty Shirt",
+                name: "Calça Maya",
                 badge: true,
                 color: ["gray", "navy", "orange"],
                 size: ["S", "M", "L"],
