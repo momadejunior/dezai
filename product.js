ba9277d99ec78f269948dee551de6ffacd5f3426
Momade Junior <momadejunior584@gmail.com>
2023-11-20 18:12:57 +0300

    Update product.js

--- product.js
+++ product.js
@@ -1,30 +1,30 @@
 let products = [
-    {
-        name: "Long Cartigen",
-        badge: true,
-        color: ["abbey", "blue", "Copper", "Old Rose"],
-        size: ["XL", "L", "M", "S"],
-        images: ["img-01.png", "img-02.png", "img-03.png", "img-04.png"],
-    }, 
-    {
-        name: "Long Cartigen",
-        badge: false,
-        color: ["abbey", "blue", "Copper", "Old Rose"],
-        size: ["XL", "L", "M", "S"],
-        images: ["img-01.png", "img-02.png", "img-03.png", "img-04.png"],
-    },
-    {
-        name: "Short Cartigen",
-        badge: true,
-        color: ["red", "green", "black", "white"],
-        size: ["M", "L", "XL"],
-        images: ["img-05.png", "img-06.png", "img-07.png", "img-08.png"],
-    },
-    {
-        name: "Sporty Shirt",
-        badge: true,
-        color: ["gray", "navy", "orange"],
-        size: ["S", "M", "L"],
-        images: ["img-09.png", "img-10.png", "img-11.png", "img-12.png"],
-    }
-];
+            {
+                name: "Long Cartigen",
+                badge: true,
+                color: ["abbey", "blue", "Copper", "Old Rose"],
+                size: ["XL", "L", "M", "S"],
+                images: ["img-01.png", "img-02.png", "img-03.png", "img-04.png"],
+                price: 100,
+                description:"longe cartigen for cold day...",
+                
+            },
+            {
+                name: "Short Cartigen",
+                badge: false,
+                color: ["red", "green", "black", "white"],
+                size: ["M", "L", "XL"],
+                images: ["img-05.png", "img-06.png", "img-07.png", "img-08.png"],
+                price: 56,
+                 description:"longe cartigen for cold day...",
+            },
+            {
+                name: "Sporty Shirt",
+                badge: true,
+                color: ["gray", "navy", "orange"],
+                size: ["S", "M", "L"],
+                images: ["img-09.png", "img-10.png", "img-11.png", "img-12.png"],
+                price: 90,
+                description:"longe cartigen for cold day...",
+            }
+        ];
