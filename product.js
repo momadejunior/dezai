583fef317cc9831758aedda5395dc1e6baf53b4e
Momade Junior <momadejunior584@gmail.com>
2023-11-20 13:11:29 +0300

    Create product.js

---
+++ product.js
@@ -0,0 +1,30 @@
+let products = [
+    {
+        name: "Long Cartigen",
+        badge: true,
+        color: ["abbey", "blue", "Copper", "Old Rose"],
+        size: ["XL", "L", "M", "S"],
+        images: ["img-01.png", "img-02.png", "img-03.png", "img-04.png"],
+    }, 
+    {
+        name: "Long Cartigen",
+        badge: false,
+        color: ["abbey", "blue", "Copper", "Old Rose"],
+        size: ["XL", "L", "M", "S"],
+        images: ["img-01.png", "img-02.png", "img-03.png", "img-04.png"],
+    },
+    {
+        name: "Short Cartigen",
+        badge: true,
+        color: ["red", "green", "black", "white"],
+        size: ["M", "L", "XL"],
+        images: ["img-05.png", "img-06.png", "img-07.png", "img-08.png"],
+    },
+    {
+        name: "Sporty Shirt",
+        badge: true,
+        color: ["gray", "navy", "orange"],
+        size: ["S", "M", "L"],
+        images: ["img-09.png", "img-10.png", "img-11.png", "img-12.png"],
+    }
+];
